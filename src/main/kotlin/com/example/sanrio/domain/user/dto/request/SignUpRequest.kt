c871ddad956e31962339f6a486b71dda2be4706d
hunzz <hunzz.study@gmail.com>
2024-08-07 18:19:00 +0900

    [Feat] 회원가입 기능 구현
    - UserService와 UserController에 signup 메서드 생성
    - 사용자가 입력한 회원가입 정보를 받아올 SignupRequest DTO 클래스 생성 (Validation 적용)
    - UserService 내 이메일 중복 여부를 체크하는 메서드(validateEmailDuplication) 생성
        - UserRepository에 이메일 존재 여부를 찾는 existsByEmail 메서드 추가
    - UserService 내 두 개의 비밀번호 일치 여부를 체크하는 메서드(validateTwoPasswords) 생성
    - SignupRequest 내 DTO 객체를 엔티티로 변환해주는 to 메서드 생성
        - PasswordEncoder를 활용한 password 암호화
        - NicknameGenerator를 활용한 랜덤 닉네임 부여

---
+++ src/main/kotlin/com/example/sanrio/domain/user/dto/request/SignUpRequest.kt
@@ -0,0 +1,37 @@
+package com.example.sanrio.domain.user.dto.request
+
+import com.example.sanrio.domain.user.model.User
+import com.example.sanrio.global.utility.NicknameGenerator.generateNickname
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+import org.springframework.context.annotation.Description
+import org.springframework.security.crypto.password.PasswordEncoder
+
+data class SignUpRequest(
+    @field:NotBlank(message = "이메일은 필수 입력 항목입니다.")
+    @field:Email(message = "올바르지 않은 이메일 형식입니다.")
+    val email: String,
+
+    @field:NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
+    @field:Pattern(
+        message = "올바르지 않은 비밀번호 형식입니다. (8~16자의 알파벳 대소문자, 숫자, 특수문자로 구성)",
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@\$!%*?&])[A-Za-z\\d@\$!%*?&]{8,16}\$"
+    )
+    val password: String,
+
+    @field:NotBlank(message = "한 번 더 비밀번호를 입력해주세요.")
+    val password2: String,
+
+    @field:NotBlank(message = "이름은 필수 입력 항목입니다.")
+    val name: String
+) {
+    @Description("DTO -> User 엔티티로 변환하는 메서드")
+    fun to(passwordEncoder: PasswordEncoder) = User(
+        email = this.email,
+        password = passwordEncoder.encode(this.password),
+        name = this.name,
+        nickname = generateNickname(),
+        address = null
+    )
+}
--- src/main/kotlin/com/example/sanrio/domain/user/service/UserService.kt
+++ src/main/kotlin/com/example/sanrio/domain/user/service/UserService.kt
@@ -1,11 +1,31 @@
 package com.example.sanrio.domain.user.service
 
+import com.example.sanrio.domain.user.dto.request.SignUpRequest
 import com.example.sanrio.domain.user.repository.UserRepository
+import com.example.sanrio.global.exception.case.DuplicatedValueException
+import com.example.sanrio.global.exception.case.InvalidValueException
+import org.springframework.context.annotation.Description
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder
 ) {
+    @Description("회원가입 시 입력한 이메일에 대한 중복 체크")
+    private fun validateEmailDuplication(email: String) =
+        if (userRepository.existsByEmail(email)) throw DuplicatedValueException("이메일") else Unit
 
+    @Description("회원가입 시 입력한 두 비밀번호가 일치하는지 체크")
+    private fun validateTwoPasswords(password1: String, password2: String) =
+        if (password1 != password2) throw InvalidValueException("비밀번호") else Unit
+
+    @Description("회원가입")
+    fun signup(request: SignUpRequest) =
+        validateEmailDuplication(email = request.email) // 이메일 검증
+            .let { validateTwoPasswords(password1 = request.password, password2 = request.password2) } // 패스워드 검증
+            .let { request.to(passwordEncoder = passwordEncoder) } // DTO -> 엔티티
+            .let { userRepository.save(it) } // 저장
+            .let { } // 리턴값 X
 }
