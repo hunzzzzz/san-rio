42edaff05c1f0559183ad0fcd5cd62a29da8aa82
hunzz <hunzz.study@gmail.com>
2024-08-08 11:06:14 +0900

    [Feat] ProductResponse, ProductDetailResponse에 status와 characterName 추가
    - ProductResponse, ProductDetailResponse에 status와 characterName 추가

--- src/main/kotlin/com/example/sanrio/domain/product/dto/response/ProductDetailResponse.kt
+++ src/main/kotlin/com/example/sanrio/domain/product/dto/response/ProductDetailResponse.kt
@@ -1,23 +1,28 @@
 package com.example.sanrio.domain.product.dto.response
 
 import com.example.sanrio.domain.product.model.Product
+import com.example.sanrio.domain.product.model.ProductStatus
 import java.time.LocalDateTime
 
 data class ProductDetailResponse(
     val productId: Long,
+    val status: ProductStatus,
     val name: String,
     val detail: String,
     val price: Int,
     val stock: Int?,
+    val characterName: String,
     val createdAt: LocalDateTime
 ) {
     companion object {
         fun from(product: Product) = ProductDetailResponse(
             productId = product.id!!,
+            status = product.status,
             name = product.name,
             detail = product.detail,
             price = product.price,
             stock = if (product.stock < 10) product.stock else null,
+            characterName = product.character.name,
             createdAt = product.createdAt
         )
     }
--- src/main/kotlin/com/example/sanrio/domain/product/dto/response/ProductResponse.kt
+++ src/main/kotlin/com/example/sanrio/domain/product/dto/response/ProductResponse.kt
@@ -1,10 +1,13 @@
 package com.example.sanrio.domain.product.dto.response
 
+import com.example.sanrio.domain.product.model.ProductStatus
 import java.time.LocalDateTime
 
 data class ProductResponse(
     val productId: Long,
+    val status: ProductStatus,
     val name: String,
     val price: Int,
+    val characterName: String,
     val createdAt: LocalDateTime
 )
