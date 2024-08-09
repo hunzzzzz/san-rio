34d8ff4b23b5de8a50193b38af59dbea830005ea
hunzz <hunzz.study@gmail.com>
2024-08-09 10:53:27 +0900

    [Feat] ItemException 생성 및 하위 2개의 에러 케이스 추가
    - 장바구니 속 상품과 관련된 모든 Exception들의 상위 클래스인 ItemException 생성 후, ExceptionHandler에 추가
    - TooManyItemsException(6개 이상의 상품을 주문한 경우) 생성
    - OutOfStockException(재고보다 더 많은 수량의 상품을 주문한 경우) 생성

---
+++ src/main/kotlin/com/example/sanrio/global/exception/case/ItemException.kt
@@ -0,0 +1,3 @@
+package com.example.sanrio.global.exception.case
+
+open class ItemException(message: String) : RuntimeException(message)
---
+++ src/main/kotlin/com/example/sanrio/global/exception/case/TooManyItemsException.kt
@@ -0,0 +1,3 @@
+package com.example.sanrio.global.exception.case
+
+class TooManyItemsException : ItemException("최대 5개의 상품만 주문할 수 있습니다.")
