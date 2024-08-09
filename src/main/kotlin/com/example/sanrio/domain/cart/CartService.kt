baa51a9375be6471b2dac77c66fd4b006405d26f
hunzz <hunzz.study@gmail.com>
2024-08-09 11:00:32 +0900

    [Feat] 장바구니에 상품을 추가하는 기능 구현
    - CartService와 CartController에 addItems 메서드 추가
    - CartRepository에 user 객체로 Cart 엔티티를 찾는 findByUser 메서드 생성

--- src/main/kotlin/com/example/sanrio/domain/cart/CartService.kt
+++ src/main/kotlin/com/example/sanrio/domain/cart/CartService.kt
@@ -1,12 +1,28 @@
 package com.example.sanrio.domain.cart
 
+import com.example.sanrio.domain.cart.model.CartItem
 import com.example.sanrio.domain.cart.repository.CartItemRepository
 import com.example.sanrio.domain.cart.repository.CartRepository
+import com.example.sanrio.global.exception.case.OutOfStockException
+import com.example.sanrio.global.exception.case.TooManyItemsException
+import com.example.sanrio.global.utility.EntityFinder
+import org.springframework.context.annotation.Description
 import org.springframework.stereotype.Service
 
 @Service
 class CartService(
     private val cartItemRepository: CartItemRepository,
-    private val cartRepository: CartRepository
+    private val cartRepository: CartRepository,
+    private val entityFinder: EntityFinder
 ) {
+    @Description("장바구니에 상품 추가")
+    fun addItems(userId: Long, productId: Long, count: Int) {
+        val product = entityFinder.findProductById(productId = productId)
+        val cart = cartRepository.findByUser(user = entityFinder.findUserById(userId = userId))
+
+        check(count <= 5) { throw TooManyItemsException() } // 최대 5개의 동일 상품만 담을 수 있다.
+        check(count <= product.stock) { throw OutOfStockException(stock = product.stock) } // 재고를 초과하는 수량을 담을 수 없다.
+
+        CartItem(count = count, product = product, cart = cart).let { cartItemRepository.save(it) }
+    }
 }
--- src/main/kotlin/com/example/sanrio/domain/cart/repository/CartRepository.kt
+++ src/main/kotlin/com/example/sanrio/domain/cart/repository/CartRepository.kt
@@ -1,8 +1,11 @@
 package com.example.sanrio.domain.cart.repository
 
 import com.example.sanrio.domain.cart.model.Cart
+import com.example.sanrio.domain.user.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CartRepository : JpaRepository<Cart, Long>
+interface CartRepository : JpaRepository<Cart, Long> {
+    fun findByUser(user: User): Cart
+}
