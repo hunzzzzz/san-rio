aaf10c17cb52f4ee444446cedc480b8e9132163b
hunzz <hunzz.study@gmail.com>
2024-08-10 10:19:22 +0900

    [Feat] Order, OrderItem 생성
    - Order, OrderItem 엔티티 생성
    - Order와 Product는 다대다 관계이므로, 중간 엔티티인 OrderItem을 만들어 각 엔티티와 다대일 관계를 갖는다.
    - Order와 User는 다대일 관계를 갖는다.
    - 주문 상태를 표현하는 OrderStatus enum 클래스를 생성

---
+++ src/main/kotlin/com/example/sanrio/domain/order/model/Order.kt
@@ -0,0 +1,28 @@
+package com.example.sanrio.domain.order.model
+
+import com.example.sanrio.domain.user.model.User
+import com.example.sanrio.global.model.BaseEntity
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "orders")
+class Order(
+    @Column(name = "code", nullable = false)
+    val code: String,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    val status: OrderStatus = OrderStatus.PAID,
+
+    @Column(name = "total_price", nullable = false)
+    val totalPrice: Int = 0,
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    val user: User
+) : BaseEntity() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id", nullable = false, unique = true)
+    val id: Long? = null
+}
