af6f53ea4370937ef61ee086fd1fda786b1be422
hunzz <hunzz.study@gmail.com>
2025-04-14 16:48:13 +0900

    user-service 내 ExceptionHandler 수정

--- user-service/src/main/kotlin/com/hunzz/userservice/utility/exception/ExceptionHandler.kt
+++ user-service/src/main/kotlin/com/hunzz/userservice/utility/exception/ExceptionHandler.kt
@@ -4,32 +4,39 @@ import com.hunzz.userservice.utility.exception.custom.InvalidUserInfoException
 import com.hunzz.userservice.utility.exception.custom.LoginException
 import com.hunzz.userservice.utility.exception.custom.UserNotFoundException
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class ExceptionHandler {
     @ExceptionHandler(value = [UserNotFoundException::class])
-    fun handleUserNotFoundException(e: UserNotFoundException): ErrorResponse {
-        return ErrorResponse(
+    fun handleUserNotFoundException(e: UserNotFoundException): ResponseEntity<ErrorResponse> {
+        val response = ErrorResponse(
             statusCode = HttpStatus.BAD_REQUEST,
             message = e.message ?: ""
         )
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response)
     }
 
     @ExceptionHandler(value = [LoginException::class])
-    fun handleLoginException(e: LoginException): ErrorResponse {
-        return ErrorResponse(
+    fun handleLoginException(e: LoginException): ResponseEntity<ErrorResponse> {
+        val response = ErrorResponse(
             statusCode = HttpStatus.BAD_REQUEST,
             message = e.message ?: ""
         )
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response)
     }
 
     @ExceptionHandler(value = [InvalidUserInfoException::class])
-    fun handleInvalidUserInfoException(e: InvalidUserInfoException): ErrorResponse {
-        return ErrorResponse(
+    fun handleInvalidUserInfoException(e: InvalidUserInfoException): ResponseEntity<ErrorResponse> {
+        val response = ErrorResponse(
             statusCode = HttpStatus.BAD_REQUEST,
             message = e.message ?: ""
         )
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response)
     }
 }
